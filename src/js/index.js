de519bb5e0c0d3b763d4e6e9a3e78a2fdf800c99
ryomurai <rzdb3693@gmail.com>
2022-02-09 01:06:49 +0900

    raisetech 模写終了

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,38 @@
-$(window).on('scroll', function(){
+// $(window).on('scroll', function(){
+//     var scrollTop = $(window).scrollTop();
+//     var bgPosition = scrollTop / 3;
+//     if(bgPosition){
+//       $('.main_v_layer1').css('background-position', 'center -'+(75+ (-bgPosition)) + 'px');
+//     }
+// });
 
-    var scrollTop = $(window).scrollTop();
-    var bgPosition = scrollTop / 4;
-    if(bgPosition){
-      $('.main_v_layer1').css('background-position', 'center -'+(75+ (-bgPosition)) + 'px');
-    }
-  });
+
+$('.main_menu').hover(
+  function() {
+    $(".sub_menu:not(:animated)", this).slideDown(300);
+  },
+  function() {
+    $(".sub_menu", this).slideUp(300);
+  }
+);
+
+$('.nav_btn a').on('click',function(e){
+  e.preventDefault();
+  $('nav').slideToggle(300);
+  if( $('.nav_btn a').hasClass('close')){
+    console.log("hogehoge");
+    $('.nav_btn a').removeClass('close');
+    $('.nav_btn a').addClass('open');
+  }else {
+    console.log("hoge");
+    $('.nav_btn a').removeClass('open');
+    $('.nav_btn a').addClass('close');
+  }
+});
+
+$('.main_menu').mouseover(function(e) {
+  $(this).find('.menu_trigger').addClass("open");
+})
+$('.main_menu').mouseout(function(e) {
+  $(this).find('.menu_trigger').removeClass("open");
+})
